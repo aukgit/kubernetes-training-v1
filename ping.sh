5d2cd517eca4de08263acd4a34c992a7c0c71a6a
Md. Alim Ul Karim <devorg.bd@gmail.com>
2025-01-11 12:06:41 +0000

    Changes by Alim Ul Karim
    https://alimkarim.com | https://riseup-pro.com
    
    Md Alim Ul Karim

--- ping.sh
+++ ping.sh
@@ -7,11 +7,12 @@ get_current_ip() {
      echo $(hostname -I | awk '{print $1}')
 }
 
-echo "Hostname:"
+echo "Getting the host name and ip"
 echo 
+echo "Hostname:"
 get_current_host_name
+
 echo 
 echo "Current Ip:"
-echo 
 get_current_ip
 echo 
